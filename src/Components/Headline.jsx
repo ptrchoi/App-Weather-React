d2999d28f12bb89efd3a7210ff5bc907a623f49b
Peter Choi <ptrchoi@gmail.com>
2020-09-02 14:48:31 -0500

    added date and time display to Headline

--- src/Components/Headline.jsx
+++ src/Components/Headline.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 
 // Headline Component Class
 class Headline extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.getDate = this.getDate.bind(this);
+  }
+  getDate() {
+    return new Date().toDateString();
+  }
+  getTime() {
+    return new Date().toLocaleTimeString();
+  }
   render(props) {
     const { city, country } = this.props.location;
     // console.log('city: ', city);
@@ -11,6 +22,8 @@ class Headline extends React.Component {
     return (
       <div>
         <p>{city}, {country}</p>
+        <p>{this.getDate()}</p>
+        <p>{this.getTime()}</p>
       </div>
     );
   }
