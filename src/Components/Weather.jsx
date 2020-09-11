c774cbd3e3f8129b7873d43796a8797135f03648
Peter Choi <ptrchoi@gmail.com>
2020-09-10 23:45:06 -0500

    1st pass functionality/testing of swapping between Summary & Details left and right

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,5 +1,6 @@
 // LIBRARIES
 import React from 'react';
+import $ from 'jquery';
 
 // UTILITY FUNCTIONS
 import {
@@ -49,11 +50,13 @@ class Weather extends React.Component {
       },
       dayForecast: null,
       hourForecast: null,
+      weatherView: 'summary',
     };
 
     this.getCoordsFromDevice = this.getCoordsFromDevice.bind(this);
     this.handleFindLoc = this.handleFindLoc.bind(this);
     this.updateUnits = this.updateUnits.bind(this);
+    this.swipeContent = this.swipeContent.bind(this);
   }
   componentDidMount() {
     // Get device geolocation
@@ -139,6 +142,105 @@ class Weather extends React.Component {
   handleFindLoc() {
     this.getCoordsFromDevice();
   }
+  swipeContent() {
+    let { weatherView } = this.state;
+
+    // let summaryDivs = $('.summary-div');
+    // let detailsDivs = $('.details-div');
+    let s1 = $('#summary1');
+    let s2 = $('#summary2');
+    let d1 = $('#details1');
+    let d2 = $('#details2');
+
+    // console.log('summaryDivs: ', summaryDivs);
+    // console.log('detailsDivs: ', detailsDivs);
+
+    if (weatherView === 'summary') {
+      // console.log('swapping from Summary to Details');
+      // console.log('summaryDivs[0]: ', summaryDivs[0]);
+      // console.log('summaryDivs[1]: ', summaryDivs[1]);
+      // console.log('detailsDivs[0]: ', detailsDivs[0]);
+      // console.log('detailsDivs[1]: ', detailsDivs[1]);
+
+      // summaryDivs[0].removeClass('swipe-in--right');
+      // summaryDivs[1].removeClass('swipe-in--right');
+      // summaryDivs[0].addClass('swipe-out--left');
+      // summaryDivs[1].addClass('swipe-out--left');
+
+      // detailsDivs[0].removeClass('swipe-out--right');
+      // detailsDivs[1].removeClass('swipe-out--right');
+      // detailsDivs[0].addClass('swipe-in--left');
+      // detailsDivs[1].addClass('swipe-in--left');
+
+      // summaryDivs.forEach((el) => {
+      //   el.removeClass('swipe-in--right');
+      //   el.addClass('swipe-out--left');
+      // });
+      // detailsDivs.forEach((el) => {
+      //   el.removeClass('swipe-out--right');
+      //   el.addClass('swipe-in--left');
+      // });
+
+      // console.log('s1: ', s1);
+      // console.log('s2: ', s2);
+      // console.log('d1: ', d1);
+      // console.log('d2: ', d2);
+
+      s1.removeClass('swipe-in--right');
+      s2.removeClass('swipe-in--right');
+      s1.addClass('swipe-out--left');
+      s2.addClass('swipe-out--left');
+
+      d1.removeClass('swipe-out--right');
+      d2.removeClass('swipe-out--right');
+      d1.addClass('swipe-in--left');
+      d2.addClass('swipe-in--left');
+
+      weatherView = 'details';
+    } else {
+      // console.log('swapping from Details to Summary');
+
+      // summaryDivs[0].removeClass('swipe-out--left');
+      // summaryDivs[1].removeClass('swipe-out--left');
+      // summaryDivs[0].addClass('swipe-in--right');
+      // summaryDivs[1].addClass('swipe-in--right');
+
+      // detailsDivs[0].removeClass('swipe-in--left');
+      // detailsDivs[1].removeClass('swipe-in--left');
+      // detailsDivs[0].addClass('swipe-out--right');
+      // detailsDivs[1].addClass('swipe-out--right');
+
+      // summaryDivs.forEach((el) => {
+      //   el.removeClass('swipe-out--left');
+      //   el.addClass('swipe-in--right');
+      // });
+      // detailsDivs.forEach((el) => {
+      //   el.removeClass('swipe-in--left');
+      //   el.addClass('swipe-out--right');
+      // });
+
+      // console.log('s1: ', s1);
+      // console.log('s2: ', s2);
+      // console.log('d1: ', d1);
+      // console.log('d2: ', d2);
+
+      s1.removeClass('swipe-out--left');
+      s2.removeClass('swipe-out--left');
+      s1.addClass('swipe-in--right');
+      s2.addClass('swipe-in--right');
+
+      d1.removeClass('swipe-in--left');
+      d2.removeClass('swipe-in--left');
+      d1.addClass('swipe-out--right');
+      d2.addClass('swipe-out--right');
+
+      weatherView = 'summary';
+    }
+
+    this.setState({
+      weatherView: weatherView,
+    });
+  }
   render() {
     let {
       units,
@@ -175,7 +277,7 @@ class Weather extends React.Component {
             </button>
           </p>
         </div>
-        <div className="weather-summary-container">
+        <div id="summary1" className="weather-summary-container summary-div">
           <p>{description}</p>
           <p>
             <i className="fas fa-long-arrow-alt-up"></i>
@@ -188,13 +290,29 @@ class Weather extends React.Component {
           <p>
             <i className="fas fa-umbrella"></i> {precProb}%
           </p>
+          <button className="tempButton" onClick={this.swipeContent}>
+            <i className="fas fa-chevron-right"></i>
+          </button>
         </div>
-        <div className="weather-details-container">
+        <div id="details1" className="weather-details-container details-div">
           <p>Humidity: {humidity}%</p>
           <p>UV Index: {uvi}</p>
+          <button className="tempButton" onClick={this.swipeContent}>
+            <i className="fas fa-chevron-left"></i>
+          </button>
         </div>
-        <ForecastDay dayForecast={dayForecast} units={units} />
-        <ForecastHour hourForecast={hourForecast} units={units} />
+        <div
+          id="summary2"
+          className="weather-day-fc-compact-container summary-div"
+        >
+          <ForecastDay dayForecast={dayForecast} units={units} />
+        </div>
+        <div
+          id="details2"
+          className="weather-hour-fc-compact-container details-div"
+        >
+          <ForecastHour hourForecast={hourForecast} units={units} />
+        </div>
       </div>
     );
   }
