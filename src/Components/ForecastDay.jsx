dd09e2dcabe8eacbabd9283669ea1d98b253fbd5
Peter Choi <ptrchoi@gmail.com>
2020-09-23 15:31:46 -0500

    code-clean up after refactor using horizontal scroll vs expand/collapse

--- src/Components/ForecastDay.jsx
+++ src/Components/ForecastDay.jsx
@@ -10,8 +10,6 @@ import C from '../constants';
 
 // LOCAL CONSTS
 const DAYS_OF_THE_WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-// const FORECAST_DAYS_COMPACT = 8; // Includes today
-// const FORECAST_DAYS_EXPANDED = 8; // 7 day forecast includes today = 8
 const FORECAST_DAYS = 8; // 7 day forecast includes today = 8
 
 // LOCAL FUNCTIONS
@@ -34,11 +32,9 @@ class ForecastDay extends React.Component {
     this.state = {
       units: 'F',
       dailyForecast: [],
-      size: 'compact',
     };
 
     this.updateForecast = this.updateForecast.bind(this);
-    // this.resize = this.resize.bind(this);
   }
   componentDidUpdate(prevProps) {
     if (this.props != prevProps && this.props.dayForecast) {
@@ -46,19 +42,9 @@ class ForecastDay extends React.Component {
     }
   }
   updateForecast(forecastData, units) {
-    // let { size } = this.state;
-
-    // // If resized, size should be manually toggled as state props will not have been updated yet
-    // if (resize) size === 'compact' ? (size = 'expanded') : (size = 'compact');
-
     let forecastArr = [];
-    let numOfDays = FORECAST_DAYS;
 
-    // if (size === 'expanded') numOfDays = FORECAST_DAYS_EXPANDED;
-
-    // Create arr of dayObjs populated with forecastData & days of the week
-    // If units in 'C', convertTemp(), else just round off the value for the default 'F' value
-    for (let i = 0; i < numOfDays; i++) {
+    for (let i = 0; i < FORECAST_DAYS; i++) {
       let dayObj = {
         day: '',
         temp: formatTemp(forecastData[i].temp.day, units),
@@ -82,22 +68,18 @@ class ForecastDay extends React.Component {
     let { dailyForecast } = this.state;
     if (!dailyForecast) return;
 
-    let classList = 'day day-compact';
-    // if (size === 'expanded') classList = 'day day-expanded';
-
     // Curry function to .map method
-    function displayDiv(classList) {
+    function displayDays() {
       return function (day) {
         return (
-          <div key={uuidv4()} className={classList}>
+          <div key={uuidv4()} className="day-forecast">
             <p className="item item-dayOfWeek">{day.day}</p>
-            <p className="item item-temp">
+            <p className="item item-temperature">
               <span className="icon-weather">
                 <i className={day.iconMapping}></i>
               </span>
-              <span className="text-degree">{day.temp}&deg;</span>
+              <span className="text-degrees">{day.temp}&deg;</span>
             </p>
-
             <div className="item item-rain">
               <div className="wrapper-raindrop-icon">
                 <span className="raindrop-under fas fa-tint"></span>
@@ -123,23 +105,10 @@ class ForecastDay extends React.Component {
       };
     }
 
-    return dailyForecast.map(displayDiv(classList));
+    return dailyForecast.map(displayDays());
   }
   render(props) {
-    // let { size } = this.state;
-    // let containerList = 'forecastDay-container';
-    let wrapperList = 'days-wrapper';
-
-    // if (size === 'expanded') {
-    //   containerList += ' expanded';
-    //   wrapperList += ' expanded-wrapper';
-    // }
-
-    return (
-      // <div className={containerList}>
-      <div className="days-wrapper">{this.renderDays()}</div>
-      // </div>
-    );
+    return <div className="forecast-container">{this.renderDays()}</div>;
   }
 }
 export default ForecastDay;
