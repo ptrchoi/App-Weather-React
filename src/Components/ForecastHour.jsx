cc2894d7bddb6141f1b6e9daaffc7f9c271eff3a
Peter Choi <ptrchoi@gmail.com>
2020-09-23 15:52:11 -0500

    code clean up from horizontal scroll refactor

--- src/Components/ForecastHour.jsx
+++ src/Components/ForecastHour.jsx
@@ -9,8 +9,6 @@ import { formatTemp, convertTime, getRainIconFillHeight } from './../Utils';
 import C from '../constants';
 
 // LOCAL CONSTS
-// const FORECAST_HOURS_COMPACT = 8;
-// const FORECAST_HOURS_EXPANDED = 16;
 const FORECAST_HOURS = 24;
 
 // ForecastHour COMPONENT CLASS
@@ -20,29 +18,15 @@ class ForecastHour extends React.Component {
 
     this.state = {
       hourlyForecast: [],
-      // size: 'compact',
     };
 
     this.updateForecast = this.updateForecast.bind(this);
-    // this.resize = this.resize.bind(this);
   }
   componentDidUpdate(prevProps) {
     if (this.props != prevProps && this.props.hourForecast)
       this.updateForecast(this.props.hourForecast);
   }
-  // updateForecast(forecastData, resize = false) {
   updateForecast(forecastData) {
-    // let { size } = this.state;
-
-    // If resized, size should be manually toggled as state props will not have been updated yet
-    // if (resize) {
-    //   if (size === 'compact') size = 'expanded';
-    //   else size = 'compact';
-    // }
-
-    // let numOfHours = FORECAST_HOURS_COMPACT;
-    // if (size === 'expanded') numOfHours = FORECAST_HOURS_EXPANDED;
-
     // Get a subset of the forecast's hourly array based on compact/expanded
     let forecastArr = forecastData.slice(0, FORECAST_HOURS);
 
@@ -50,32 +34,11 @@ class ForecastHour extends React.Component {
       hourlyForecast: forecastArr,
     });
   }
-  // resize() {
-  //   if (!this.props.hourForecast) return;
-
-  //   let { size } = this.state;
-
-  //   if (size === 'compact') size = 'expanded';
-  //   else size = 'compact';
-
-  //   this.updateForecast(this.props.hourForecast, true);
-  //   this.props.onResize(size);
-
-  //   this.setState({
-  //     size: size,
-  //   });
-  // }
   renderForecast(units) {
-    // let { hourlyForecast, size } = this.state;
     let { hourlyForecast } = this.state;
     if (hourlyForecast.length <= 0) return;
 
-    // let classList = 'hour-compact';
-
-    // if (size === 'expanded') classList = 'hour-expanded';
-
     // Curry function to .map method
-    // function displayDiv(units, classList) {
     function displayHours(units) {
       return function (hour) {
         let timeArr = convertTime(hour.dt); // timeArr = [ hour, ':', mins, daytime]
@@ -86,7 +49,6 @@ class ForecastHour extends React.Component {
         if (rainObj) rainfall = rainObj[Object.keys(rainObj)[0]];
 
         return (
-          // <div key={uuidv4()} className={classList}>
           <div key={uuidv4()} className="hour-forecast">
             <p className="item item-hour ">
               {timeArr[0]}
@@ -120,7 +82,6 @@ class ForecastHour extends React.Component {
       };
     }
 
-    // return hourlyForecast.map(displayDiv(units, classList));
     return hourlyForecast.map(displayHours(units));
   }
   render(props) {
@@ -131,27 +92,4 @@ class ForecastHour extends React.Component {
     );
   }
 }
-//   render(props) {
-//     let { size } = this.state;
-//     let classList = 'forecastHour-container';
-//     let wrapperList = 'hours-wrapper';
-
-//     if (size === 'expanded') {
-//       classList += ' expanded'; // Defined in forecasthour.scss
-//       wrapperList += ' expanded-wrapper';
-//     }
-//     return (
-//       <div className={classList}>
-//         <div className="forecast-btn">
-//           <button className="resize-btn" onClick={this.resize}>
-//             <i className="fas fa-expand"></i>
-//           </button>
-//         </div>
-//         <div className={wrapperList}>
-//           {this.renderForecast(this.props.units)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 export default ForecastHour;
