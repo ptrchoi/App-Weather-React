5b67a3290fc5cfa722f2d4621053b99041ffe218
Peter Choi <ptrchoi@gmail.com>
2020-09-25 23:59:30 -0500

    added orientation adjustment when landscape rotation; refactor usage of vh to %

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // LIBRARIES
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import 'regenerator-runtime/runtime';
 
 // COMPONENTS
@@ -14,9 +15,34 @@ import './styles/weather-icons.scss';
 
 // APP COMPONENT CLASS
 class App extends React.Component {
+  componentDidMount() {
+    // Set initial orientation
+    this.adjustOrientation(window.orientation);
+    // Function to counter device rotation to maintain portrait perspective
+    window.addEventListener(
+      'orientationchange',
+      () => this.adjustOrientation(window.orientation),
+      true
+    );
+  }
+  adjustOrientation(orientation) {
+    if (orientation === -90) {
+      $('.app-container').addClass('orientright');
+      $('.app-container').removeClass('orientleft');
+    }
+    if (orientation === 90) {
+      $('.app-container').addClass('orientleft');
+      $('.app-container').removeClass('orientright');
+    }
+    if (orientation === 0) {
+      $('.app-container').removeClass('orientleft');
+      $('.app-container').removeClass('orientright');
+    }
+  }
+
   render() {
     return (
-      <div className="app-container">
+      <div id="app" className="app-container">
         <Weather />
       </div>
     );
