f745573ff4b40c0f0f95382b14be12036c6404c4
Peter Choi <ptrchoi@gmail.com>
2020-10-06 17:38:42 -0500

    styling of InfoModal content

--- src/Components/InfoModal.jsx
+++ src/Components/InfoModal.jsx
@@ -9,10 +9,57 @@ function InfoModal(props) {
   return (
     <div className={modalOnOff}>
       <div className="sidebar">
-        <ul className="info-text">
-          <li>Weather data from OpenWeather.com</li>
-          <li>Images from Unsplash.com</li>
-          <li>Designed and developed by Peter Choi</li>
+        <div className="info-heading">
+          <p className="app-title">Weather App</p>
+          <p className="app-subtitle">
+            designed and developed by{' '}
+            <a href="https://ptrchoi.com" target="_blank">
+              Peter Choi
+            </a>
+          </p>
+        </div>
+        <ul className="fa-ul">
+          <li>
+            <span className="fa-li">
+              <i className="fas fa-umbrella"></i>
+            </span>
+            Weather data from{' '}
+            <a href="https://openweathermap.org/" target="_blank">
+              OpenWeather.com
+            </a>{' '}
+            API
+          </li>
+          <li>
+            <span className="fa-li">
+              <i className="fas fa-images"></i>
+            </span>
+            Images from{' '}
+            <a href="https://unsplash.com/" target="_blank">
+              Unsplash.com
+            </a>{' '}
+            API
+          </li>
+          <li>
+            <span className="fa-li">
+              <i className="fas fa-cloud-sun-rain"></i>
+            </span>
+            Weather Icon library by{' '}
+            <a
+              href="https://erikflowers.github.io/weather-icons/"
+              target="_blank"
+            >
+              Erik Flowers
+            </a>
+          </li>
+          <li>
+            <span className="fa-li">
+              <i className="fas fa-palette"></i>
+            </span>
+            Weather Icon art by{' '}
+            <a href="https://twitter.com/artill" target="_blank">
+              Lukas Bischoff
+            </a>
+          </li>
         </ul>
       </div>
     </div>
