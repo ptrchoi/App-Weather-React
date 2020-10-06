1cef4cf33811ab94a52d68f14eb5a4d17703f199
Peter Choi <ptrchoi@gmail.com>
2020-10-06 16:47:35 -0500

    sidebar with animation and content added

--- src/Components/InfoModal.jsx
+++ src/Components/InfoModal.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 function InfoModal(props) {
   let { modalOpen } = props;
-  const modalOnOff = modalOpen ? 'modal-container' : 'modal-container hidden';
+  const modalOnOff = modalOpen
+    ? 'modal-container modal-container--open'
+    : 'modal-container';
 
-  return <div className={modalOnOff}></div>;
+  return (
+    <div className={modalOnOff}>
+      <div className="sidebar">
+        <ul className="info-text">
+          <li>Weather data from OpenWeather.com</li>
+          <li>Images from Unsplash.com</li>
+          <li>Designed and developed by Peter Choi</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default InfoModal;
