503227a8c556eaf8f689afb399c894aae93cf442
Peter Choi <ptrchoi@gmail.com>
2020-10-07 13:13:24 -0500

    info modal style tuning for Mobile

--- src/Components/InfoModal.jsx
+++ src/Components/InfoModal.jsx
@@ -12,53 +12,60 @@ function InfoModal(props) {
         <div className="info-heading">
           <p className="app-title">Weather App</p>
           <p className="app-subtitle">
-            designed and developed by{' '}
+            created by{' '}
             <a href="https://ptrchoi.com" target="_blank">
               Peter Choi
             </a>
           </p>
         </div>
+        <span className="horizontal-divider"></span>
         <ul className="fa-ul">
           <li>
-            <span className="fa-li">
+            <span className="bullet-wrapper fa-li">
               <i className="fas fa-umbrella"></i>
             </span>
-            Weather data from{' '}
-            <a href="https://openweathermap.org/" target="_blank">
-              OpenWeather.com
-            </a>{' '}
-            API
+            <span className="li-text">
+              Weather data -{' '}
+              <a href="https://openweathermap.org/" target="_blank">
+                OpenWeather.com
+              </a>
+            </span>
           </li>
           <li>
-            <span className="fa-li">
+            <span className="bullet-wrapper fa-li">
               <i className="fas fa-images"></i>
             </span>
-            Images from{' '}
-            <a href="https://unsplash.com/" target="_blank">
-              Unsplash.com
-            </a>{' '}
-            API
+            <span className="li-text">
+              Images -{' '}
+              <a href="https://unsplash.com/" target="_blank">
+                Unsplash.com
+              </a>
+            </span>
           </li>
           <li>
-            <span className="fa-li">
+            <span className="bullet-wrapper fa-li">
               <i className="fas fa-cloud-sun-rain"></i>
             </span>
-            Weather Icon library by{' '}
-            <a
-              href="https://erikflowers.github.io/weather-icons/"
-              target="_blank"
-            >
-              Erik Flowers
-            </a>
+            <span className="li-text">
+              Weather Icons -{' '}
+              <a
+                href="https://erikflowers.github.io/weather-icons/"
+                target="_blank"
+              >
+                Erik Flowers
+              </a>
+            </span>
           </li>
           <li>
-            <span className="fa-li">
+            <span className="bullet-wrapper fa-li">
               <i className="fas fa-palette"></i>
             </span>
-            Weather Icon art by{' '}
-            <a href="https://twitter.com/artill" target="_blank">
-              Lukas Bischoff
-            </a>
+            <span className="li-text">
+              Weather Icon art -{' '}
+              <a href="https://twitter.com/artill" target="_blank">
+                Lukas Bischoff
+              </a>
+            </span>
           </li>
         </ul>
       </div>
